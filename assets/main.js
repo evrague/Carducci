711dd388e8b3676cc5044835779ce003d7f413f1
évrague <jpfoucaux@gmail.com>
2025-01-23 19:22:21 +0100

    deploiement de site

---
+++ assets/main.js
@@ -0,0 +1,23 @@
+let tous=document.getElementById("tous")
+let entreprises=document.getElementById("Entreprises")
+let mariages=document.getElementById("Mariages")
+let portrait=document.getElementById("Portrait")
+let concert=document.getElementById("Concert")
+
+concert.addEventListener("click", () =>{
+    concert.classList.add("active-tag")
+})
+entreprises.addEventListener("click", () =>{
+    entreprises.classList.add("active-tag")
+})
+mariages.addEventListener("click)", () =>{
+    mariages.classList.add("active-tag")
+})
+portrait.addEventListener("click",() =>{
+    portrait.classList.add("active-tag")
+})
+concert.addEventListener("click", () =>{
+    concert.classList.add("active-tag")
+})
+
+
