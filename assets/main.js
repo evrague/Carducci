2510b1dab790681165f0c058ee983e8cfff3ba38
évrague <jpfoucaux@gmail.com>
2025-01-29 20:29:46 +0100

    Les erreurs de navigateur ont été enregistrées dans la console

--- assets/main.js
+++ assets/main.js
@@ -1,23 +1,17 @@
-let tous=document.getElementById("tous")
-let entreprises=document.getElementById("Entreprises")
-let mariages=document.getElementById("Mariages")
-let portrait=document.getElementById("Portrait")
-let concert=document.getElementById("Concert")
+$(document).ready(function() {
+    $("#Entreprises").click(function() {
+        $(this).addClass("active-tag");
+    });
 
-concert.addEventListener("click", () =>{
-    concert.classList.add("active-tag")
-})
-entreprises.addEventListener("click", () =>{
-    entreprises.classList.add("active-tag")
-})
-mariages.addEventListener("click)", () =>{
-    mariages.classList.add("active-tag")
-})
-portrait.addEventListener("click",() =>{
-    portrait.classList.add("active-tag")
-})
-concert.addEventListener("click", () =>{
-    concert.classList.add("active-tag")
-})
+    $("#Mariages").click(function() {
+        $(this).addClass("active-tag");
+    });
 
+    $("#Portrait").click(function() {
+        $(this).addClass("active-tag");
+    });
 
+    $("#Concert").click(function() {
+        $(this).addClass("active-tag");
+    });
+});
